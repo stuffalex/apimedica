8660fa9198a1f4d3674582e754a42a1b14bd7997
AlexyaMoraes <alexyavianaa@gmail.com>
2025-11-15 14:50:19 -0400

    Config corsconfiguration.java para poder criar o front

---
+++ src/main/java/med/voll/api/CorsConfiguration.java
@@ -0,0 +1,17 @@
+package med.voll.api;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+//configuração para poder receber requisições do front-end
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+    }
+}
